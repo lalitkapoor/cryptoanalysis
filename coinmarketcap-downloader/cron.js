04579eabebb41bc11bf8e3132accb4c9d6498cd4
Lalit Kapoor <lalitkapoor@gmail.com>
2017-12-29 20:53:27 -0500

    add price-momentum-10-moderate strategy and run in parallel

--- coinmarketcap-downloader/cron.js
+++ coinmarketcap-downloader/cron.js
@@ -3,6 +3,7 @@ import uuid from 'uuid'
 import db from '@/lib/db'
 import Worker from './worker'
 import PriceMomentumLast10ConservativeStrategy from '@/strategies/price-momentum/last-10-conservative'
+import PriceMomentumLast10ModerateStrategy from '@/strategies/price-momentum/last-10-moderate'
 
 new cron.CronJob('0 */1 * * * *', async () => {
   const id = uuid.v4()
@@ -14,9 +15,16 @@ new cron.CronJob('0 */1 * * * *', async () => {
   console.log(new Date(), id, 'importing complete')
 
   // run strategies
-  const strategy = new PriceMomentumLast10ConservativeStrategy()
-  console.log(new Date(), id, 'running strategy:', strategy.name)
-  await strategy.run()
-  console.log(new Date(), id, 'done running strategy:', strategy.name)
-  console.log('\n\n')
+  const strategies = [
+    new PriceMomentumLast10ConservativeStrategy(),
+    new PriceMomentumLast10ModerateStrategy()
+  ]
+
+  strategies.forEach((strategy) => {
+    console.log(new Date(), id, 'running strategy:', strategy.name)
+    strategy.run().catch(console.log)
+    console.log(new Date(), id, 'done running strategy:', strategy.name)
+    console.log('\n\n')
+  })
+
 }, null, true, 'America/New_York')
---
+++ coinmarketcap-downloader/db/sql/last-10-moderate.sql
@@ -0,0 +1,163 @@
+WITH
+filtered AS (
+  SELECT * FROM cmc_market_data WHERE ticker NOT IN (
+    SELECT ticker
+    FROM cmc_market_data
+    WHERE
+      last_updated > now() - interval '3 hours' AND
+      (
+      market_cap_usd < 1000000 OR
+      price_usd IS NULL OR
+      market_cap_usd IS NULL)
+  )
+  ORDER BY last_updated DESC
+),
+
+last_3 AS (
+  SELECT *
+  FROM (
+    SELECT
+      4 - ROW_NUMBER() OVER (
+        PARTITION BY ticker
+        ORDER BY ticker ASC, last_updated DESC
+      ) AS row_num,
+      filtered.*
+    FROM filtered
+  ) numbered
+  WHERE
+    numbered.row_num <= 3 AND
+    numbered.row_num > 0
+  ORDER BY ticker, row_num DESC
+),
+
+last_5 AS (
+  SELECT *
+  FROM (
+    SELECT
+      6 - ROW_NUMBER() OVER (
+        PARTITION BY ticker
+        ORDER BY ticker ASC, last_updated DESC
+      ) AS row_num,
+      filtered.*
+    FROM filtered
+  ) numbered
+  WHERE
+    numbered.row_num <= 5 AND
+    numbered.row_num > 0
+  ORDER BY ticker, row_num DESC
+),
+
+last_10 AS (
+  SELECT *
+  FROM (
+    SELECT
+      11 - ROW_NUMBER() OVER (
+        PARTITION BY ticker
+        ORDER BY ticker ASC, last_updated DESC
+      ) AS row_num,
+      filtered.*
+    FROM filtered
+  ) numbered
+  WHERE
+    numbered.row_num <= 10 AND
+    numbered.row_num > 0
+  ORDER BY ticker, row_num DESC
+),
+
+percent_change_3 AS (
+  SELECT
+    row_num,
+    ticker,
+    last_updated,
+    price_usd,
+    market_cap_usd,
+    (price_usd - lead(price_usd) OVER (PARTITION BY ticker ORDER BY row_num DESC)) / lead(price_usd) OVER (PARTITION BY ticker ORDER BY row_num DESC) * 100 as percent_change
+  FROM last_3
+),
+
+percent_change_5 AS (
+  SELECT
+    row_num,
+    ticker,
+    last_updated,
+    price_usd,
+    market_cap_usd,
+    (price_usd - lead(price_usd) OVER (PARTITION BY ticker ORDER BY row_num DESC)) / lead(price_usd) OVER (PARTITION BY ticker ORDER BY row_num DESC) * 100 as percent_change
+  FROM last_5
+),
+
+percent_change_10 AS (
+  SELECT
+    row_num,
+    ticker,
+    last_updated,
+    price_usd,
+    market_cap_usd,
+    (price_usd - lead(price_usd) OVER (PARTITION BY ticker ORDER BY row_num DESC)) / lead(price_usd) OVER (PARTITION BY ticker ORDER BY row_num DESC) * 100 as percent_change
+  FROM last_10
+),
+
+percent_change_3_counts AS (
+  SELECT ticker, COUNT(*) AS cnt
+  FROM percent_change_3
+  WHERE
+    percent_change_3.percent_change > 0 AND
+    percent_change_3.percent_change <= 50
+  GROUP BY ticker
+  ORDER BY cnt DESC
+),
+
+percent_change_5_counts AS (
+  SELECT ticker, COUNT(*) AS cnt
+  FROM percent_change_5
+  WHERE
+    percent_change_5.percent_change >= 3 AND
+    percent_change_5.percent_change <= 50
+  GROUP BY ticker
+  ORDER BY cnt DESC
+),
+
+percent_change_10_counts AS (
+  SELECT ticker, COUNT(*) AS cnt
+  FROM percent_change_10
+  WHERE
+    percent_change_10.percent_change >= 5 AND
+    percent_change_10.percent_change <= 50
+  GROUP BY ticker
+  ORDER BY cnt DESC
+),
+
+final_analysis AS (
+  SELECT
+    percent_change_10_counts.ticker,
+    percent_change_10_counts.cnt AS cnt_10,
+    percent_change_5_counts.cnt AS cnt_5,
+    percent_change_3_counts.cnt AS cnt_3
+  FROM percent_change_10_counts
+  LEFT JOIN percent_change_3_counts USING (ticker)
+  LEFT JOIN percent_change_5_counts USING (ticker)
+)
+
+SELECT
+  f.ticker,
+  f.symbol,
+  f.rank,
+  f.price_usd,
+  f.market_cap_usd,
+  f.volume_24h_usd,
+  f.last_updated  AT TIME ZONE 'America/New_York' AS last_updated,
+  final_analysis.cnt_10,
+  final_analysis.cnt_5,
+  final_analysis.cnt_3
+FROM final_analysis
+INNER JOIN (
+  SELECT DISTINCT ON (ticker) *
+  FROM filtered
+  ORDER BY ticker, last_updated DESC
+) f USING(ticker)
+WHERE
+  f.last_updated > now() - interval '10 minutes' AND -- if no update in the last 10 min, ignore
+  cnt_10 >= 3 AND
+  cnt_5 >= 1 AND
+  cnt_3 >= 1
+ORDER BY cnt_10 DESC, cnt_5 DESC, cnt_3 DESC
