dfcbbffc21e46d61211eb92d33314a8073b5e8d6
Lalit Kapoor <lalitkapoor@gmail.com>
2018-01-04 22:29:10 -0500

    clean up

--- coinmarketcap-downloader/db/sql/analysis/rank-momentum-past-6-hours.sql
+++ coinmarketcap-downloader/db/sql/analysis/rank-momentum-past-6-hours.sql
@@ -1,6 +1,6 @@
 WITH
 
-snap_6h_ago AS (
+time_6h_ago AS (
   SELECT DISTINCT ON (ticker, symbol) *
   FROM cmc_market_data
   WHERE
@@ -12,7 +12,7 @@ snap_6h_ago AS (
   ORDER BY ticker, symbol, last_updated ASC
 ),
 
-snap_5h_ago AS (
+time_5h_ago AS (
   SELECT DISTINCT ON (ticker, symbol) *
   FROM cmc_market_data
   WHERE
@@ -24,7 +24,7 @@ snap_5h_ago AS (
   ORDER BY ticker, symbol, last_updated ASC
 ),
 
-snap_4h_ago AS (
+time_4h_ago AS (
   SELECT DISTINCT ON (ticker, symbol) *
   FROM cmc_market_data
   WHERE
@@ -36,7 +36,7 @@ snap_4h_ago AS (
   ORDER BY ticker, symbol, last_updated ASC
 ),
 
-snap_3h_ago AS (
+time_3h_ago AS (
   SELECT DISTINCT ON (ticker, symbol) *
   FROM cmc_market_data
   WHERE
@@ -48,7 +48,7 @@ snap_3h_ago AS (
   ORDER BY ticker, symbol, last_updated ASC
 ),
 
-snap_2h_ago AS (
+time_2h_ago AS (
   SELECT DISTINCT ON (ticker, symbol) *
   FROM cmc_market_data
   WHERE
@@ -60,7 +60,7 @@ snap_2h_ago AS (
   ORDER BY ticker, symbol, last_updated ASC
 ),
 
-snap_1h_ago AS (
+time_1h_ago AS (
   SELECT DISTINCT ON (ticker, symbol) *
   FROM cmc_market_data
   WHERE
@@ -86,7 +86,7 @@ latest AS (
 
 earliest AS (
   SELECT *
-  FROM snap_6h_ago
+  FROM time_6h_ago
 ),
 
 final_analysis AS (
@@ -99,58 +99,80 @@ final_analysis AS (
     latest.volume_24h_usd,
     latest.last_updated  AT TIME ZONE 'America/New_York' AS last_updated,
 
-    snap_6h_ago.price_usd AS snap_6h_ago_price_usd,
-    snap_5h_ago.price_usd AS snap_5h_ago_price_usd,
-    snap_4h_ago.price_usd AS snap_4h_ago_price_usd,
-    snap_3h_ago.price_usd AS snap_3h_ago_price_usd,
-    snap_2h_ago.price_usd AS snap_2h_ago_price_usd,
-    snap_1h_ago.price_usd AS snap_1h_ago_price_usd,
+    time_6h_ago.volume_24h_usd AS time_6h_ago_volume_24h_usd,
+    time_5h_ago.volume_24h_usd AS time_5h_ago_volume_24h_usd,
+    time_4h_ago.volume_24h_usd AS time_4h_ago_volume_24h_usd,
+    time_3h_ago.volume_24h_usd AS time_3h_ago_volume_24h_usd,
+    time_2h_ago.volume_24h_usd AS time_2h_ago_volume_24h_usd,
+    time_1h_ago.volume_24h_usd AS time_1h_ago_volume_24h_usd,
+    latest.volume_24h_usd AS latest_volume_24h_usd,
+
+    time_5h_ago.volume_24h_usd - time_6h_ago.volume_24h_usd AS time_6h_5h_volume_diff,
+    time_4h_ago.volume_24h_usd - time_5h_ago.volume_24h_usd AS time_5h_4h_volume_diff,
+    time_3h_ago.volume_24h_usd - time_4h_ago.volume_24h_usd AS time_4h_3h_volume_diff,
+    time_2h_ago.volume_24h_usd - time_3h_ago.volume_24h_usd AS time_3h_2h_volume_diff,
+    time_1h_ago.volume_24h_usd - time_2h_ago.volume_24h_usd AS time_2h_1h_volume_diff,
+    latest.volume_24h_usd - time_1h_ago.volume_24h_usd AS time_1h_0h_volume_diff,
+
+    ROUND(((time_5h_ago.volume_24h_usd - time_6h_ago.volume_24h_usd)/time_6h_ago.volume_24h_usd) * 100, 2) AS time_6h_5h_volume_percent_diff,
+    ROUND(((time_4h_ago.volume_24h_usd - time_5h_ago.volume_24h_usd)/time_5h_ago.volume_24h_usd) * 100, 2) AS time_5h_4h_volume_percent_diff,
+    ROUND(((time_3h_ago.volume_24h_usd - time_4h_ago.volume_24h_usd)/time_4h_ago.volume_24h_usd) * 100, 2) AS time_4h_3h_volume_percent_diff,
+    ROUND(((time_2h_ago.volume_24h_usd - time_3h_ago.volume_24h_usd)/time_3h_ago.volume_24h_usd) * 100, 2) AS time_3h_2h_volume_percent_diff,
+    ROUND(((time_1h_ago.volume_24h_usd - time_2h_ago.volume_24h_usd)/time_2h_ago.volume_24h_usd) * 100, 2) AS time_2h_1h_volume_percent_diff,
+    ROUND(((latest.volume_24h_usd - time_1h_ago.volume_24h_usd)/time_1h_ago.volume_24h_usd) * 100, 2) AS time_1h_0h_volume_percent_diff,
+
+
+    time_6h_ago.price_usd AS time_6h_ago_price_usd,
+    time_5h_ago.price_usd AS time_5h_ago_price_usd,
+    time_4h_ago.price_usd AS time_4h_ago_price_usd,
+    time_3h_ago.price_usd AS time_3h_ago_price_usd,
+    time_2h_ago.price_usd AS time_2h_ago_price_usd,
+    time_1h_ago.price_usd AS time_1h_ago_price_usd,
     latest.price_usd AS latest_price_usd,
 
-    snap_5h_ago.price_usd - snap_6h_ago.price_usd AS time_6h_5h_price_diff,
-    snap_4h_ago.price_usd - snap_5h_ago.price_usd AS time_5h_4h_price_diff,
-    snap_3h_ago.price_usd - snap_4h_ago.price_usd AS time_4h_3h_price_diff,
-    snap_2h_ago.price_usd - snap_3h_ago.price_usd AS time_3h_2h_price_diff,
-    snap_1h_ago.price_usd - snap_2h_ago.price_usd AS time_2h_1h_price_diff,
-    latest.price_usd - snap_1h_ago.price_usd AS time_1h_0h_price_diff,
+    time_5h_ago.price_usd - time_6h_ago.price_usd AS time_6h_5h_price_diff,
+    time_4h_ago.price_usd - time_5h_ago.price_usd AS time_5h_4h_price_diff,
+    time_3h_ago.price_usd - time_4h_ago.price_usd AS time_4h_3h_price_diff,
+    time_2h_ago.price_usd - time_3h_ago.price_usd AS time_3h_2h_price_diff,
+    time_1h_ago.price_usd - time_2h_ago.price_usd AS time_2h_1h_price_diff,
+    latest.price_usd - time_1h_ago.price_usd AS time_1h_0h_price_diff,
 
-    ROUND(((snap_5h_ago.price_usd - snap_6h_ago.price_usd)/snap_6h_ago.price_usd) * 100, 2) AS time_6h_5h_price_percent_diff,
-    ROUND(((snap_4h_ago.price_usd - snap_5h_ago.price_usd)/snap_5h_ago.price_usd) * 100, 2) AS time_5h_4h_price_percent_diff,
-    ROUND(((snap_3h_ago.price_usd - snap_4h_ago.price_usd)/snap_4h_ago.price_usd) * 100, 2) AS time_4h_3h_price_percent_diff,
-    ROUND(((snap_2h_ago.price_usd - snap_3h_ago.price_usd)/snap_3h_ago.price_usd) * 100, 2) AS time_3h_2h_price_percent_diff,
-    ROUND(((snap_1h_ago.price_usd - snap_2h_ago.price_usd)/snap_2h_ago.price_usd) * 100, 2) AS time_2h_1h_price_percent_diff,
-    ROUND(((latest.price_usd - snap_1h_ago.price_usd)/snap_1h_ago.price_usd) * 100, 2) AS time_1h_0h_price_percent_diff,
+    ROUND(((time_5h_ago.price_usd - time_6h_ago.price_usd)/time_6h_ago.price_usd) * 100, 2) AS time_6h_5h_price_percent_diff,
+    ROUND(((time_4h_ago.price_usd - time_5h_ago.price_usd)/time_5h_ago.price_usd) * 100, 2) AS time_5h_4h_price_percent_diff,
+    ROUND(((time_3h_ago.price_usd - time_4h_ago.price_usd)/time_4h_ago.price_usd) * 100, 2) AS time_4h_3h_price_percent_diff,
+    ROUND(((time_2h_ago.price_usd - time_3h_ago.price_usd)/time_3h_ago.price_usd) * 100, 2) AS time_3h_2h_price_percent_diff,
+    ROUND(((time_1h_ago.price_usd - time_2h_ago.price_usd)/time_2h_ago.price_usd) * 100, 2) AS time_2h_1h_price_percent_diff,
+    ROUND(((latest.price_usd - time_1h_ago.price_usd)/time_1h_ago.price_usd) * 100, 2) AS time_1h_0h_price_percent_diff,
 
-    snap_6h_ago.rank AS snap_6h_ago_rank,
-    snap_5h_ago.rank AS snap_5h_ago_rank,
-    snap_4h_ago.rank AS snap_4h_ago_rank,
-    snap_3h_ago.rank AS snap_3h_ago_rank,
-    snap_2h_ago.rank AS snap_2h_ago_rank,
-    snap_1h_ago.rank AS snap_1h_ago_rank,
+    time_6h_ago.rank AS time_6h_ago_rank,
+    time_5h_ago.rank AS time_5h_ago_rank,
+    time_4h_ago.rank AS time_4h_ago_rank,
+    time_3h_ago.rank AS time_3h_ago_rank,
+    time_2h_ago.rank AS time_2h_ago_rank,
+    time_1h_ago.rank AS time_1h_ago_rank,
     latest.rank AS latest_rank,
 
-    -1 * (snap_5h_ago.rank - snap_6h_ago.rank) AS time_6h_5h_rank_diff,
-    -1 * (snap_4h_ago.rank - snap_5h_ago.rank) AS time_5h_4h_rank_diff,
-    -1 * (snap_3h_ago.rank - snap_4h_ago.rank) AS time_4h_3h_rank_diff,
-    -1 * (snap_2h_ago.rank - snap_3h_ago.rank) AS time_3h_2h_rank_diff,
-    -1 * (snap_1h_ago.rank - snap_2h_ago.rank) AS time_2h_1h_rank_diff,
-    -1 * (latest.rank - snap_1h_ago.rank) AS time_1h_0h_rank_diff,
+    -1 * (time_5h_ago.rank - time_6h_ago.rank) AS time_6h_5h_rank_diff,
+    -1 * (time_4h_ago.rank - time_5h_ago.rank) AS time_5h_4h_rank_diff,
+    -1 * (time_3h_ago.rank - time_4h_ago.rank) AS time_4h_3h_rank_diff,
+    -1 * (time_2h_ago.rank - time_3h_ago.rank) AS time_3h_2h_rank_diff,
+    -1 * (time_1h_ago.rank - time_2h_ago.rank) AS time_2h_1h_rank_diff,
+    -1 * (latest.rank - time_1h_ago.rank) AS time_1h_0h_rank_diff,
 
-    ROUND(((latest.price_usd - snap_6h_ago.price_usd)/snap_6h_ago.price_usd) * 100, 2) AS overall_price_percent_diff,
-    -1 * (latest.rank - snap_6h_ago.rank) AS overall_rank_diff
+    ROUND(((latest.volume_24h_usd - time_6h_ago.volume_24h_usd)/time_6h_ago.volume_24h_usd) * 100, 2) AS overall_volume_percent_diff,
+    ROUND(((latest.price_usd - time_6h_ago.price_usd)/time_6h_ago.price_usd) * 100, 2) AS overall_price_percent_diff,
+    -1 * (latest.rank - time_6h_ago.rank) AS overall_rank_diff
 
   FROM latest
-  INNER JOIN snap_6h_ago USING (ticker, symbol)
-  INNER JOIN snap_5h_ago USING (ticker, symbol)
-  INNER JOIN snap_4h_ago USING (ticker, symbol)
-  INNER JOIN snap_3h_ago USING (ticker, symbol)
-  INNER JOIN snap_2h_ago USING (ticker, symbol)
-  INNER JOIN snap_1h_ago USING (ticker, symbol)
+  INNER JOIN time_6h_ago USING (ticker, symbol)
+  INNER JOIN time_5h_ago USING (ticker, symbol)
+  INNER JOIN time_4h_ago USING (ticker, symbol)
+  INNER JOIN time_3h_ago USING (ticker, symbol)
+  INNER JOIN time_2h_ago USING (ticker, symbol)
+  INNER JOIN time_1h_ago USING (ticker, symbol)
 )
 
 SELECT
   final_analysis.*
 FROM final_analysis
-WHERE
-  time_1h_0h_rank_diff > 0
 ORDER BY overall_rank_diff DESC
